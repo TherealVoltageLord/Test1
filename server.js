d2a105e193c12fd477cb8f6b61d632dec86c5b5c
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-04-28 13:02:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1935,7 +1935,7 @@ app.get('/api/health', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 // Cron Jobs
